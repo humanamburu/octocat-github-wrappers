1c222267cbc97b0fe3c7ac5a7432511376fa7c77
humanamburu <humanamburu@ojooo.com>
2017-02-21 02:12:02 +0300

    feature: adding script

--- examples/script.js
+++ examples/script.js
@@ -5,32 +5,57 @@ const {
     addRepoForTeam,
     createOrgRepo,
 } = require('./../src/OctoWrappers');
+const { students } = require('./new_students.json');
+
+const gitignore = '.idea\nnode_modules\n*.bundle.js\n*.log\n';
+const editorconfig = 'root = true\n\n[*]\nend_of_line = lf\ninsert_final_newline = true\n[*.js]\n\ncharset = utf-8\nindent_style = space\nindent_size = 4\n';
+const organization = 'rolling-scopes-school';
 
 login().then((data) => {
     logger.info('-----------------------------');
     logger.info(` Hello ${data.username}!`);
     logger.info('-----------------------------');
 
-    const organization = 'rolling-scopes-school';
+
+    createRepo(students.pop());
+});
+
+function createRepo(user) {
+    if (!user) {
+        return;
+    }
+
+    const repoName = `${user}-front-end-course`;
     const options = {
-        description: 'Test repo with collaborator',
         auto_init: true,
-        gitignore_template: 'Node',
+        gitignore_template: false,
         private: true,
         collaborator: {
-            name: 'humanamburu',
+            name: user,
             permission: 'push',
         }
     };
-    const repoName = 'humanamburu-frontend-course';
 
     createOrgRepo(organization, repoName, options)
         .then(() => {
-            return addRepoForTeam(organization, 'core', repoName, 'push').then(() => {
-                return addFile(repoName, '.eslint', 'eslint', '.idea \n node_modules', organization);
-            });
-        }).then(() => {
-            logger.warn('DONE!');
+            return addRepoForTeam(organization, 'Mentors', repoName, 'push');
         })
-        .catch((e) => logger.error(JSON.stringify(e)));
-});
+        .then(() => {
+            return addRepoForTeam(organization, 'Trainers', repoName, 'push');
+        })
+        .then(() => {
+            return addFile(repoName, '.gitignore', 'feature: base .gitiginore', gitignore, organization);
+        })
+        .then(() => {
+            return addFile(repoName, '.editorconfig', 'feature: base .editorconfig', editorconfig, organization)
+        })
+        .then(() => {
+            logger.warn(user, ' DONE!');
+            createRepo(students.pop());
+        })
+        .catch((e) => {
+            logger.error(user, JSON.stringify(e));
+            createRepo(students.pop());
+        });
+
+}
